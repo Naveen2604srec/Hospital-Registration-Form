3e606c43f0b1528c019079372ba7c1e52e67c866
Naveen2604srec <144478591+Naveen2604srec@users.noreply.github.com>
2024-08-08 02:13:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+import RegistrationForm from './components/RegistrationForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  );
+};
+
+export default App;
